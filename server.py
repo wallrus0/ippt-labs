4d7b8be94b44c7a46f66f4b5fedc9ef0d6837589
wallrus0 <54054960+wallrus0@users.noreply.github.com>
2019-11-22 20:19:20 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind (('192.168.1.1',9999))
+client = []
+print ('Start Server')
+while True :
+         data , addres = sock.recvfrom(1024)
+         print (addres[0], addres[1])
+         if  addres not in client :
+                 client.append(addres)
+         for clients in client :
+                 if clients == addres:
+                     continue
+                 sock.sendto(data,clients)
